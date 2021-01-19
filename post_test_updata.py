f97ad36a3ddf1a2d970b2713aded987e70b63731
yfq512 <1085561337@qq.com>
2021-01-19 18:18:01 +0800

    测试post

---
+++ post_test_updata.py
@@ -0,0 +1,11 @@
+#coding=utf-8
+import requests
+import time
+import base64
+import json
+
+t1 = time.time()
+s = requests
+data={'sign':0}
+#r = s.post('http://192.168.132.151:8879/txtsimilar', data)
+r = s.post('http://0.0.0.0:8886/uptxt', data)
